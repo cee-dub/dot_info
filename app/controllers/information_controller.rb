27f4a3d76cbb75fc07e0344918a4d28cbb78935a
Cameron Walters <cameron.walters@gmail.com>
2009-03-27 14:15:52 -0700

    Add information_controller and make specs work.

---
+++ app/controllers/information_controller.rb
@@ -0,0 +1,5 @@
+class InformationController < ApplicationController
+  def index
+    render :index
+  end
+end
---
+++ spec/controllers/information_controller_spec.rb
@@ -0,0 +1,10 @@
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+describe InformationController do
+
+  #Delete this example and add some real ones
+  it "should use InformationController" do
+    controller.should be_an_instance_of(InformationController)
+  end
+
+end
